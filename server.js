faa4d8351e60b98c7fde4f8a7930efc3103f3876
vi <dev.facereal@gmail.com>
2018-09-11 18:20:46 +0700

    add some packeage for serve

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
+
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname+'/client/dist'));
 
