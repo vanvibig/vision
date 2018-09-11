eabb430a63500c394972540426b0e3d4798c0511
vi <dev.facereal@gmail.com>
2018-09-11 18:17:05 +0700

    init client

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+
+app.use(express.static(__dirname+'/client/dist'));
 
 app.get('/', (req, res) => {
-    res.send('Hello from App Engine!');
+    res.sendFile(path.join(__dirname+'/client/dist/index.html'));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
