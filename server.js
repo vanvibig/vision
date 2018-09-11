52435e73e53608899173842071cc8fcc03a1ffea
vi <dev.facereal@gmail.com>
2018-09-11 08:58:49 +0700

    update

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from App Engine!');
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
+
+
 // Imports the Google Cloud client library
 const vision = require('@google-cloud/vision');
 
@@ -12,12 +26,20 @@ const client = new vision.ImageAnnotatorClient(config);
 // const fileName = 'Local image file, e.g. /path/to/image.png';
 fileName = './resources/block-of-text.png';
 // Read a local image as a text document
+
+text = '';
+
 client
     .documentTextDetection(fileName)
     .then(results => {
         const fullTextAnnotation = results[0].fullTextAnnotation;
         console.log(`Full text: ${fullTextAnnotation.text}`);
+        text = fullTextAnnotation.text;
     })
     .catch(err => {
         console.error('ERROR:', err);
     });
+
+app.get('/annotate', (req, res) => {
+    res.send(text);
+});
